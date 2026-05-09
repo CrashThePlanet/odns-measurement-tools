20a1e8ad93e399fff40c8c5e0c79affe23b8d7d8
Til Hempel <til.hempel@gmail.com>
2026-05-09 14:06:06 +0200

    postProc script to classify results wether they support qmin or not

---
+++ src/postprocessing/qmin/classify_data_pure.py
@@ -0,0 +1,125 @@
+
+import csv
+from pathlib import Path
+import sys
+from enum import Enum
+from datetime import datetime
+
+class ResponseStatus(Enum):
+    UNHANDLEDERROR = -1
+    GOOD = 0
+    REFUSED = 1
+    SERVFAIL = 2
+    TIMEOUT = 3
+    NXDOMAIN = 4
+    NOROUTE = 5
+    NORECURSION = 6
+    NOANSWER = 7
+    NOTXTRESPONSE = 8
+
+# all types (except ERROR) will contain another split for Resolver that sometimes answered (with the respective type) and sometimes gave an error
+class ResolverEval(Enum):
+    ERROR = -1
+    NOQMIN = 0
+    QMIN = 1
+    PARTIAL = 2
+
+def classify():
+    # setup empty dict structure to hold classified data
+    result = {key: None for key in ResolverEval}
+
+    for key in ResolverEval:
+        if key == ResolverEval.ERROR:
+            continue
+        
+        result[key] = {"allGood": [], "someError": []}
+    result[ResolverEval.ERROR] = []
+    
+    with open(sys.argv[1], newline='') as file:
+        csvfile = csv.DictReader(file)
+        for row in csvfile:
+            responses = stringToDict(row['response'])
+
+            someError = False
+            for key in responses.keys():
+                if key in ResponseStatus:
+                    someError = True
+            if int(row['qmin']) == -1:
+                result[ResolverEval.ERROR].append(row.values())
+            else:
+                result[ResolverEval(int(row['qmin']))]["someError" if someError else "allGood"].append(row.values())
+        
+        file.close()
+        #print(result[ResolverEval.QMIN]["allGood"])
+    return result
+
+
+def stringToDict(input):
+    input = input[1:-1]
+
+    res = {}
+
+    top = input.split(";")
+    for line in top:
+        resType = line.split(":")
+
+        if "*idToken*" in resType[0]:
+            res[resType[0]] = int(resType[1])
+        else:
+            # some resolver return blocked in form of a TXt message
+            # now skip
+            # TODO: handle
+            try:
+                res[ResponseStatus[resType[0].upper()]] = int(resType[1])
+            except:
+                res[ResponseStatus.UNHANDLEDERROR] = 1
+    return res
+
+def makeDirctory(path):
+    try:
+        path.mkdir()
+    except FileExistsError:
+        print(f"Directory '{path}' already exists.")
+        return
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{path}'.")
+        return
+    except Exception as e:
+        print(f"An error occurred while creating output directory: {e}")
+        return
+
+def writeClassifiedResolver(resolver, outDir):
+    makeDirctory(outDir)
+
+    for key, value in resolver.items():
+        if isinstance(value, dict):
+            writeClassifiedResolver(value, outDir / str(key))
+        if isinstance(value, list):
+            if len(value) <= 1:
+                continue
+            with open(outDir / (str(key)+".csv"), 'w') as outfile:
+                write = csv.writer(outfile)
+                write.writerows(value)
+                outfile.close
+
+if __name__ == '__main__':
+
+    if not Path(sys.argv[1]).exists():
+        print("File does not exist!")
+        pass
+    
+    outputDir = "./../../data/processed/qmin/"
+    if 2 < len(sys.argv):
+        if not Path(sys.argv[2]).exists():
+            print("Provided output directory does not exist")
+            pass
+        outputDir = sys.argv[2]
+    
+    outputDir = Path(outputDir + "classify_" + datetime.now().strftime("%Y-%m-%d_%H-%M"))
+
+
+    classifiedResolver = classify()
+
+    writeClassifiedResolver(classifiedResolver, outputDir)
+
+    
